1e5d459be96d346cdfb741251c36c04ad1095b08
Amir <amir-rt@live.com>
2022-03-11 17:26:55 +1100

    feat: parameters tutorial

--- main.tf
+++ main.tf
@@ -29,12 +29,30 @@ data "digitalocean_ssh_key" "mySSHKey" {
 
 resource "digitalocean_droplet" "web" {
   image    = "ubuntu-18-04-x64"
-  name     = "web-1"
-  region   = "nyc3"
-  size     = "s-1vcpu-1gb"
+  name     = "web-${var.region}-${count.index + 1}"
+  region   = var.region
+  size     = var.droplet_size
+  count    = var.droplet_count
   ssh_keys = [data.digitalocean_ssh_key.mySSHKey.id]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+output "server_ip" {
+  value = digitalocean_droplet.web.*.ipv4_address
 }
 
-output "server_ip" {
-  value = digitalocean_droplet.web.ipv4_address
+variable "region" {
+  type    = string
+  default = "nyc3"
+}
+
+variable "droplet_count" {
+  type    = number
+  default = 1
+}
+
+variable "droplet_size" {
+  type    = string
+  default = "s-1vcpu-1gb"
 }
---
+++ sfo2.tfvars
@@ -0,0 +1,4 @@
+# sfo2.tfvars
+region        = "sfo2"
+droplet_count = 1
+droplet_size  = "s-1vcpu-1gb"
